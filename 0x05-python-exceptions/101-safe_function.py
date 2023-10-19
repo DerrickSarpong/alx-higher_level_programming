5f82c00698037c791c4cb97d8ae76a7391abe7e5
DerrickSarpong <derrick.sarpong.ds@gmail.com>
2023-10-19 14:17:30 +0000

    Additional tasks

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+
+    try:
+        return fct(*args)
+    except Exception as ex:
+        print("Error: {}".format(ex), file=sys.stderr)
+        return None
